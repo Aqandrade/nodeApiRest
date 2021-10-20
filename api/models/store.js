74a4d029e88ec120f2c75d05177c1199f8b46382
Alisson Andrade <aqandrade23@gmail.com>
2021-10-20 19:42:31 -0300

    refactor: store.js with airbnb pattern

--- api/models/store.js
+++ api/models/store.js
@@ -1,21 +1,17 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class Store extends Model {
-    static associate(models) {
-    }
-  };
-  Store.init({
-    name: DataTypes.STRING,
-    price: DataTypes.DECIMAL,
-    damage: DataTypes.INTEGER,
-    minLevel: DataTypes.INTEGER,
-    active: DataTypes.ENUM
-  }, {
-    sequelize,
-    modelName: 'Store',
-  });
-  return Store;
-};
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../database');
+
+class Store extends Model {}
+
+Store.init({
+  name: DataTypes.STRING,
+  price: DataTypes.DECIMAL,
+  damage: DataTypes.INTEGER,
+  minLevel: DataTypes.INTEGER,
+  active: DataTypes.ENUM('1', '0'),
+}, {
+  sequelize,
+  modelName: 'Store',
+});
+
+module.exports = Store;
